a1dbffc7d7f2409eef32021a2021c20e5cda36a5
Neto Andrey <diomand761988@gmail.com>
2026-04-09 18:16:11 +0300

    Failed test postRequestBody

--- src/test/java/ru/netology/postmanecho/PostmanEchoTest.java
+++ src/test/java/ru/netology/postmanecho/PostmanEchoTest.java
@@ -20,6 +20,6 @@ public class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo(requestBody));
+                .body("fail", equalTo(requestBody));
     }
 }
